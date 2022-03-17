ebcd3c3745c5e62941404516283ebf55c629dfc0
rafhabarb <100421043+rafhabarb@users.noreply.github.com>
2022-03-17 12:47:45 -0300

    Add files via upload

---
+++ dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria58@hotmail.com',
+      avatarurl:
+          'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Jose',
+      email: 'jose89@hotmail.com',
+      avatarurl:
+          'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Thiago',
+      email: 'thiago21@hotmail.com',
+      avatarurl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQwWMJbZoZ26ZyYB8M-1e7OLBVUWXRLNSO6A&usqp=CAU'),
+  '4': const User(
+      id: '4',
+      name: 'Joaquina',
+      email: 'joaquina25@hotmail.com',
+      avatarurl:
+          'https://media.istockphoto.com/vectors/user-avatarbusinessman-a-businessman-in-a-suit-with-a-red-tie-and-vector-id948490174?k=20&m=948490174&s=170667a&w=0&h=dYEdcCAhs8p0GGOMO__jMhTD8PB8PVpN8p4W1Pheacw='),
+};
---
+++ user.tile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/models/user.dart';
+
+class UserTile extends StatelessWidget {
+  final User user;
+
+  const UserTile(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    final avatar = user.avatarurl == null || user.avatarurl.isEmpty
+        ? CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(backgroundImage: NetworkImage(user.avatarurl));
+    return ListTile(
+        leading: avatar,
+        title: Text(user.name),
+        subtitle: Text(user.email),
+        trailing: Container(
+          width: 100,
+          child: Row(children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              color: Colors.orange,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              color: Colors.red,
+              onPressed: () {},
+            ),
+          ]),
+        ));
+  }
+}

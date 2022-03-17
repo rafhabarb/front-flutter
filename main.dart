f001f9c6eb9bcacacb49d0bf6aa3c6481825090b
rafhabarb <100421043+rafhabarb@users.noreply.github.com>
2022-03-17 12:44:52 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/views/user_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: UserList(),
+    );
+  }
+}
